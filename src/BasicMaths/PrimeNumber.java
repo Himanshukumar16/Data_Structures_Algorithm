3a75d1943f5647380dbfdde08aa71a081710c051
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-06-28 12:06:25 +0530

    Adding Readme file.

--- src/BasicMaths/PrimeNumber.java
+++ src/BasicMaths/PrimeNumber.java
@@ -1,6 +1,7 @@
 // Prime number or not
 package BasicMaths;
 public class PrimeNumber {
+    int num = 5;
     public static void main(String[] args) {
         System.out.println("Hello World !");
     }
