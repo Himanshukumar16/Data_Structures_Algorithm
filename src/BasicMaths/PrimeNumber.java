4e28b12e4e111b73217d448af0a68893aa1317c3
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-06-28 20:18:04 +0530

    Prime NUmber Or Not.

--- src/BasicMaths/PrimeNumber.java
+++ src/BasicMaths/PrimeNumber.java
@@ -1,8 +1,19 @@
 // Prime number or not
 package BasicMaths;
+import java.util.*;
 public class PrimeNumber {
-    int num = 5;
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean out = true;
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                out = false;
+            }
+        }if(n == 1 || n == 0){
+            System.out.println("Neither even nor odd !");
+        }else{
+            System.out.println(out);
+        }
     }
 }
