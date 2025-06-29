7fb396f43003e01205075a231c4e00f22f58dbaa
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-06-29 17:22:37 +0530

    Full_Quiz_For_Practice_Java_Code.

--- src/JAVA_CORE/QuestionService.java
+++ src/JAVA_CORE/QuestionService.java
@@ -14,14 +14,14 @@ public class QuestionService {
         q[1] = new Question("Q2)."," Which of these cannot be used for a variable name in Java?","1. identifier & keyword",
                 "2. identifier","3. keyword","4. none of the mentioned","3");
 
-        q[2] = new Question("Q3)."," abcd find next","1. e",
-                "2. f","3. g","4. h","1");
+        q[2] = new Question("Q3)."," Which of the following is not an OOPS concept in Java?","1. Polymorphism",
+                "2. Inheritance","3. Encapsulation","4. Compilation","4");
 
-        q[3] = new Question("Q4)."," abcd find next","1. e",
-                "2. f","3. g","4. h","1");
+        q[3] = new Question("Q4)."," Which of the following is a type of polymorphism in Java Programming?","1. Multiple polymorphism",
+                "2. Compile time polymorphism","3. Multilevel polymorphism","4. Execution time polymorphism","2");
 
-        q[4] = new Question("Q5)."," abcd find next","1. e",
-                "2. f","3. g","4. h","1");
+        q[4] = new Question("Q5)."," Which statement is true about Java?","1. Java is a sequence-dependent programming language",
+                "2. Java is a code dependent programming language","3. Java is a platform-dependent programming language","4. Java is a platform-independent programming language","4");
 
     }
 
@@ -42,11 +42,15 @@ public class QuestionService {
             ans[i] = sc.nextLine();
 
             if((q[i].getCorrect()).equals(ans[i])){
+
                 System.out.println("Answer is correct!");
                 score += 1;
+
             }
             else {
+
                 System.out.println("you select wrong answer! Better luck next time.");
+
             }
         }
     }
