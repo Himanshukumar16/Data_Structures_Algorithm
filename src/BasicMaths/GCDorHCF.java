ce85ffc3911f5ee2d1f90171066b2195714dddc2
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-06-30 10:10:30 +0530

    BASIC_MATH_COMPLETED.

--- src/BasicMaths/GCDorHCF.java
+++ src/BasicMaths/GCDorHCF.java
@@ -2,6 +2,16 @@
 package BasicMaths;
 public class GCDorHCF {
     public static void main(String[] args) {
-
+        int a = 20;
+        int b = 54;
+        while(a > 0 && b > 0){
+            if(a>b){
+                a = a % b;
+            }
+            else{
+                b = b % a;
+            }
+        }
+        System.out.println(Math.max(a,b));
     }
 }
