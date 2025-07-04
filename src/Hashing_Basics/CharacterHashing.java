539db31566a203ac0a1d3ce7daaba7fe22db4835
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-04 11:23:19 +0530

    RECURSSION_COMPLETED.

--- src/Hashing_Basics/CharacterHashing.java
+++ src/Hashing_Basics/CharacterHashing.java
@@ -6,20 +6,22 @@ public class CharacterHashing {
 ////        Brute-Force approach:
         Scanner sc = new Scanner(System.in);
 
-//        String str = "abcdabef";
-//        int count = 0;
-//
-//        char check = sc.next().charAt(0);
-//
-//        for (int i = 0; i < str.length();i++){
-//
-//            if(str.charAt(i) == check){
-//
-//                count++;
-//
-//            }
-//        }
-//
-//        System.out.println(count);
+/*
+        String str = "abcdabef";
+        int count = 0;
+
+        char check = sc.next().charAt(0);
+
+        for (int i = 0; i < str.length();i++){
+
+            if(str.charAt(i) == check){
+
+                count++;
+
+            }
+        }
+
+        System.out.println(count);
+*/
     }
 }
--- src/Recursion/PallindromeString.java
+++ src/Recursion/PallindromeString.java
@@ -1,4 +1,4 @@
-// Palindrome String.
+// Palindrome String...
 package Recursion;
 public class PallindromeString {
     public static void main(String[] args) {
