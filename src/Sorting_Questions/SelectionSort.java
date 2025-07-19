6e72c220a2459ec9efe131c182e6a75efd89985b
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-19 16:03:55 +0530

    Selection_Sorting

--- src/Sorting_Questions/SelectionSort.java
+++ src/Sorting_Questions/SelectionSort.java
@@ -1,7 +1,29 @@
+// Selection sort !
 package Sorting_Questions;
-
 public class SelectionSort {
     public static void main(String[] args) {
 
+        int[]arr = {-1,-11,-1,1,2,2,3,5};
+
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = i+1; j < arr.length; j++){
+
+                int min = arr[i];
+
+                if(min > arr[j]){
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+        }
+
+        for (int i : arr) {
+
+            System.out.println(i);
+
+        }
     }
 }
