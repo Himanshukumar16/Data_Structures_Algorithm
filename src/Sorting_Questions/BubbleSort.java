f5fdc7c383672a68f0ad56ca9b7c39c3dd61b47a
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-07-20 23:20:40 +0530

    Selection_Sorting_And_Bubble_Sorting.

--- src/Sorting_Questions/BubbleSort.java
+++ src/Sorting_Questions/BubbleSort.java
@@ -3,12 +3,23 @@ package Sorting_Questions;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] arr ={10,12,5,6,2,23};
+        int[] arr ={10,22,5,6,2,23};
 
-        for(int i = 0; i < arr.length ; i++) {
-            for(int j = i+1 ; j < arr.length ; j++){
+        for(int i = 0; i < arr.length - 1 ; i++) {
+            for(int j = 0 ; j <= i ; j++){
 
+                if(arr[j] > arr[i]){
+
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
+        }
+
+        for(int i : arr){
+
+            System.out.print(i + " ");
         }
     }
 }
