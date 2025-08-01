67cd4af40209d50aae6bf0258b9cc7daa7b72c53
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-01 23:33:27 +0530

    Practice_Question

--- src/SelfPractice/GFG_Questions/BasicMaths_3.java
+++ src/SelfPractice/GFG_Questions/BasicMaths_3.java
@@ -4,19 +4,20 @@ import java.util.*;
 
 public class BasicMaths_3 {
     public void gcdAndLcm() {
-//        int[] arr = new int[2];
-//        int a = 10 , b = 20;
-//        while(a !=0 && b !=0) {
-//            if (a > b) {
-//                a = a % b;
-//            } else {
-//                b = b % a;
-//            }
-//        }
-//        arr[1] = Math.max(a,b);
-//        for(int i:arr){
-//            System.out.print(i+" ");
-//        }
+        int[] arr = new int[2];
+
+        int a = 10, b = 20;
+        while (a != 0 && b != 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        arr[1] = Math.max(a, b);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
 
