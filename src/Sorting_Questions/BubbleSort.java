2c9ec61cfcbf4e22ff679e366887d07eb91962ca
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-01 22:47:05 +0530

    Sorting

--- src/Sorting_Questions/BubbleSort.java
+++ src/Sorting_Questions/BubbleSort.java
@@ -3,19 +3,28 @@ package Sorting_Questions;
 public class BubbleSort {
     public static void main(String[] args) {
 
-        int[] arr ={10,22,5,6,2,23};
+        int[] arr = {120,0,25,23,2};
 
-        for(int i = 0; i < arr.length - 1 ; i++) {
-            for(int j = 0 ; j <= i ; j++){
+//        for(int i = 1; i < arr.length - 1 ; i++) {
+//            for(int j = 0 ; j < arr.length - 1; j++){
+//
+//                if(arr[j] > arr[i]){
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+//        }
 
-                if(arr[j] > arr[i]){
-
+        for(int i = 0; i < arr.length-1 ; i++){
+//            for(int j = 0; j < arr.length; j++){
+                if(arr[i+1] > arr[i]){
                     int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
                 }
             }
-        }
+//        }
 
         for(int i : arr){
 
