6ebc726d6d362faf0374fb4429e4e897d5a35218
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-06 00:23:37 +0530

    Merge_Sort_Added.

--- src/Sorting_Part_2/Merge_Sort.java
+++ src/Sorting_Part_2/Merge_Sort.java
@@ -1,51 +1,51 @@
-package Sorting_Part_2;
 
+package Sorting_Part_2;
 public class Merge_Sort {
     public static void main(String[] args) {
         Sol s = new Sol();
-        int[] arr = {5, 4, 3, 2, 10};
-        s.sortMerge(arr, 0, 4);
+        int [] arr = {5,2,3,1};
+        s.sortMerge( arr,0, arr.length - 1);
+        for ( int k : arr ){
+            System.out.println(k + " ");
+        }
     }
 }
-
-class Sol {
-    public void sortMerge(int[] arr, int low, int high) {
-        if (low >= high) return;
-        int mid = (low + high) / 2;
-        sortMerge(arr, low, mid);
-        sortMerge(arr, mid + 1, high);
-        merge(arr, low, mid, high);
+class Sol{
+    public void sortMerge( int [] arr, int low , int high ){
+        if ( low >= high ) return;
+        int mid = ( low + high ) / 2;
+        sortMerge( arr , low , mid );
+        sortMerge( arr , mid + 1, high );
+        merge( arr , low , mid , high );
     }
-
-    public void merge(int[] arr, int low, int mid, int high) {
-        int[] temp = new int[arr.length];
+    public void merge(int [] arr , int low , int mid , int high){
+        int [] temp = new int[arr.length];
         int i = 0;
         int left = low;
         int right = mid + 1;
-        while (left <= mid && right <= high) {
-            if (left < right) {
-                temp[i] = left;
+        while(left <= mid && right <= high){
+            if(arr[left] < arr[right]){
+                temp[i] = arr[left];
                 left++;
                 i++;
-            } else {
-                temp[i] = right;
+            }else{
+                temp[i] = arr[right];
                 right++;
                 i++;
             }
         }
-        while (left <= mid) {
-            temp[i] = left;
+        while (left <= mid){
+            temp[i] = arr[left];
             left++;
             i++;
         }
-        while (right <= high) {
-            temp[i] = right;
+        while (right <= high){
+            temp[i] = arr[right];
             right++;
             i++;
         }
-        if (high - low >= 0) System.arraycopy(temp, 0, arr, low, high - low);
-        for (int k : arr) {
-            System.out.println(k + " ");
+        for (int j = low; j <= high; j++) {
+            arr[j] = temp[j - low];
         }
     }
 }
