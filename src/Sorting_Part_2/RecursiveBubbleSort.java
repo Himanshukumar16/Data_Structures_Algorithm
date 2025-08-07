87cd8f0004da03aa6b9fdfed0bcab7fc02409860
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-07 23:16:52 +0530

    Recursive_Bubble_Sort_Added.

--- src/Sorting_Part_2/RecursiveBubbleSort.java
+++ src/Sorting_Part_2/RecursiveBubbleSort.java
@@ -3,7 +3,7 @@ package Sorting_Part_2;
 public class RecursiveBubbleSort {
     public static void main(String[] args) {
         Recur r = new Recur();
-        int[] arr = {15, 44, 63, 52, 91};
+        int[] arr = {151, 414, 163, 512, 911};
         r.recur(arr, arr.length);
         for (int i : arr) {
             System.out.println(i);
@@ -16,7 +16,7 @@ class Recur {
         if (length == 1) {
             return;
         }
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
                 int temp = arr[i];
                 arr[i] = arr[i + 1];
