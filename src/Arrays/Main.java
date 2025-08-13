19d0254a382eb058552ac44ccc42b4c44b44606b
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-14 00:22:37 +0530

    Arrays_Added.

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        Question 1 : O(n)
-        LargestElementInAnArray l = new LargestElementInAnArray();
-        l.largeNumber(new int[]{3, 2, 1, 4, 6, 5});
+//        LargestElementInAnArray l = new LargestElementInAnArray();
+//        l.largeNumber(new int[]{3, 2, 1, 4, 6, 5});
 
 //        Question 2 :
         SecondLargestElementInArray sl = new SecondLargestElementInArray();
-        sl.secondLargest(new int[]{3, 2, 1, 4, 6, 5});
+        sl.secondLargest(new int[]{11,22,33,44,55,66,852});
     }
 }
--- src/Arrays/SecondLargestElementInArray.java
+++ src/Arrays/SecondLargestElementInArray.java
@@ -6,17 +6,25 @@ public class SecondLargestElementInArray {
 
 //        TC -> O(2n). (Better approach.)
 
-//        int slarge = Integer.MIN_VALUE;
-//        int large = arr[0];
-//        for(int i = 0; i < arr.length; i++){
-//            if(arr[i] > large) large = arr[i];
-//        }
-//        for (int i = 0; i < arr.length ; i++){
-//            if(arr[i] > slarge && arr[i] != large ) slarge = arr[i];
-//        }
-//        System.out.println("Second Largest Number In The Array Is : "+slarge);
+        int slarge = Integer.MIN_VALUE;
+        int large = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > large) large = arr[i];
+        }
+        for (int i = 0; i < arr.length ; i++){
+            if(arr[i] > slarge && arr[i] != large ) slarge = arr[i];
+        }
+        System.out.println("Second Largest Number In The Array Is : "+slarge);
 
-//        TC -> O() (Optimal approach.)
+//        TC -> O(n) (Optimal approach.)
 
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > large) {
+                slarge = large;
+                large = arr[i];
+            }
+            else if(arr[i] < large && arr[i] > slarge) slarge = arr[i];
+        }
+        System.out.println("Second Largest Number In The Array Is : "+slarge);
     }
 }
