d5d449be631c5627a1d93abd09e3c9f07f520d2f
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-13 23:30:29 +0530

    Arrays_Added.

--- src/Arrays/LargestElementInAnArray.java
+++ src/Arrays/LargestElementInAnArray.java
@@ -1,5 +1,11 @@
 package Arrays;
 
 public class LargestElementInAnArray {
-
+    public void largeNumber(int[]arr){
+        int large = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > large) large = arr[i];
+        }
+        System.out.println("The Largest Number In Array Is : "+large);
+    }
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,7 +1,10 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        LargestElementInAnArray l = new LargestElementInAnArray();
+        l.largeNumber(new int[]{3, 2, 1, 4, 6, 5});
     }
 }
