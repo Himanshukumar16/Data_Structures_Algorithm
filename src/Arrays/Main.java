79deaf946fcf4953bbd86ecd43f67c96dcdd0ff5
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-08-15 01:33:57 +0530

    Arrays_Practice.

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        Question 1 : O(n)
-        LargestElementInAnArray l = new LargestElementInAnArray();
-        l.largeNumber(new int[]{3, 2, 1, 4, 6, 5});
+//        LargestElementInAnArray l = new LargestElementInAnArray();
+//        l.largeNumber(new int[]{3, 2, 1, 4, 6, 5});
 
 //        Question 2 :
-        SecondLargestElementInArray sl = new SecondLargestElementInArray();
-        sl.secondLargest(new int[]{3, 2, 1, 4, 6, 5});
+//        SecondLargestElementInArray sl = new SecondLargestElementInArray();
+//        sl.secondLargest(new int[]{3, 2, 1, 4, 6, 5});
 
 //        Question 3 :
-        IsSortedOrNot is = new IsSortedOrNot();
-        System.out.println(is.isSorted(new int[]{0,2,5,9,70}));
+//        IsSortedOrNot is = new IsSortedOrNot();
+//        System.out.println(is.isSorted(new int[]{0,2,5,9,70}));
 
 //        Question 4 :
-        RemoveDuplicateFromSortedArray d = new RemoveDuplicateFromSortedArray();
-        System.out.println(d.removeDuplicateFromSortedArray(new int[]{-3,-1,0,0,0,3,3}));
+//        RemoveDuplicateFromSortedArray d = new RemoveDuplicateFromSortedArray();
+//        System.out.println(d.removeDuplicateFromSortedArray(new int[]{-3,-1,0,0,0,3,3}));
 
 //        Question 5 :
         LeftRotateTheArrayByOnePlace r = new LeftRotateTheArrayByOnePlace();
