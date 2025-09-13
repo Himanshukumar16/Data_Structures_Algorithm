bc6136817ead6b6c6877b9787a9bf2bd8a52de2e
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-09-13 19:46:27 +0530

    Arrays_Practice.

--- src/Arrays_Easy_Medium_Hard/MaximumFrequency.java
+++ src/Arrays_Easy_Medium_Hard/MaximumFrequency.java
@@ -5,19 +5,44 @@ import java.util.Map;
 
 public class MaximumFrequency {
     public void maximumFrequency(String str) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < str.length(); i++) {
-            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
-        }
+
+
+//        Brute-Force : TC -> O(n^2), SC -> O(1)...
+
         int maxVol = 0;
         int maxCons = 0;
-        for (char i : map.keySet()) {
-            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
-                maxVol = Math.max(maxVol, map.get(i));
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == c) {
+                    count++;
+                }
+            }
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                maxVol = Math.max(maxVol, count);
             } else {
-                maxCons = Math.max(maxCons, map.get(i));
+                maxCons = Math.max(maxCons, count);
             }
         }
         System.out.println(maxVol + maxCons);
+
+
+//        Optimal : TC -> O(n), SC -> O(1)...
+
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < str.length(); i++) {
+//            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+//        }
+//        int maxVol = 0;
+//        int maxCons = 0;
+//        for (char i : map.keySet()) {
+//            if (i == 'a' || i == 'e' || i == 'i' || i == 'o' || i == 'u') {
+//                maxVol = Math.max(maxVol, map.get(i));
+//            } else {
+//                maxCons = Math.max(maxCons, map.get(i));
+//            }
+//        }
+//        System.out.println(maxVol + maxCons);
     }
 }
