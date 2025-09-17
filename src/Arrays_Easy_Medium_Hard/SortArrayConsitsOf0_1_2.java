65f07219ccf5ce98492fa2d794c48d5ee50da87d
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-09-17 21:57:38 +0530

    Arrays_Practice.

--- src/Arrays_Easy_Medium_Hard/SortArrayConsitsOf0_1_2.java
+++ src/Arrays_Easy_Medium_Hard/SortArrayConsitsOf0_1_2.java
@@ -3,7 +3,7 @@ package Arrays_Easy_Medium_Hard;
 public class SortArrayConsitsOf0_1_2 {
     public void sortArrayConsitsOf0_1_2(int arr[]) {
 
-//        Brute-Force TC -> O(nlogn), SC -> O(n).
+//        Brute-Force TC -> O(nlogn), SC -> O(n)....
 
 //        Better :
 
