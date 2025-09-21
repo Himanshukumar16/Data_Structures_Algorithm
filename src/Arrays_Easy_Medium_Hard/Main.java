ccdadc930bf46daac28732280da257467f5779fd
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-09-21 23:32:09 +0530

    Arrays_Practice.

--- src/Arrays_Easy_Medium_Hard/Main.java
+++ src/Arrays_Easy_Medium_Hard/Main.java
@@ -113,12 +113,13 @@ public class Main {
 //        t.twoSum(new int[]{2,5,3,4}, 9);
 
 //        Question 14 :
-        SortArrayConsitsOf0_1_2 s = new SortArrayConsitsOf0_1_2();
-        s.sortArrayConsitsOf0_1_2(new int[]{2, 0, 2, 1, 1, 0, 0, 0, 1});
-
+//        SortArrayConsitsOf0_1_2 s = new SortArrayConsitsOf0_1_2();
+//        s.sortArrayConsitsOf0_1_2(new int[]{0, 2, 1, 0, 2, 1, 1, 2, 0, 0, 1});
 
 //        Leetcode 3541 :
 //        MaximumFrequency m = new MaximumFrequency();
 //        m.maximumFrequency("successes");
+
+//
     }
 }
--- src/Arrays_Easy_Medium_Hard/SortArrayConsitsOf0_1_2.java
+++ src/Arrays_Easy_Medium_Hard/SortArrayConsitsOf0_1_2.java
@@ -7,29 +7,51 @@ public class SortArrayConsitsOf0_1_2 {
 
 //        Better :
 
-        int count1 = 0;
-        int count2 = 0;
-        int count0 = 0;
+//        int count1 = 0;
+//        int count2 = 0;
+//        int count0 = 0;
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] == 0) count0++;
+//            if (arr[i] == 1) count1++;
+//            if (arr[i] == 2) count2++;
+//        }
+//        for (int i = 0; i < count0; i++) {
+//            arr[i] = 0;
+//        }
+//        for (int i = count0; i < count0 + count1; i++) {
+//            arr[i] = 1;
+//        }
+//        for (int i = count0 + count1; i < arr.length; i++) {
+//            arr[i] = 2;
+//        }
+//        for (int i : arr) {
+//            System.out.println(i);
+//        }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) count0++;
-            if (arr[i] == 1) count1++;
-            if (arr[i] == 2) count2++;
-        }
-        for (int i = 0; i < count0; i++) {
-            arr[i] = 0;
-        }
-        for (int i = count0; i < count0 + count1; i++) {
-            arr[i] = 1;
-        }
-        for (int i = count0 + count1; i < arr.length; i++) {
-            arr[i] = 2;
+//        Optimal : TC->O(),SC->O()...
+
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else if (arr[mid] == 2) {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
         }
         for (int i : arr) {
             System.out.println(i);
         }
-
-//        Optimal : TC->O(),SC->O()...
-
     }
 }
