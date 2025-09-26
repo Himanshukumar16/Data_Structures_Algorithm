7ba9c8139ca05b59db26077e6bd46e886af8ad92
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-09-26 14:28:27 +0530

    Arrays_Practice.

--- src/Arrays_Easy_Medium_Hard/MajorityElement.java
+++ src/Arrays_Easy_Medium_Hard/MajorityElement.java
@@ -30,7 +30,6 @@ public class MajorityElement {
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        System.out.println(map);
         for (int i : map.keySet()) {
             if (map.get(i) > nums.length / 2) {
                 System.out.println(i);
