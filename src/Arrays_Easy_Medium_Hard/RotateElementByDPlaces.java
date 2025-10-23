217426c61e81364a3b68b96a57477b7266572446
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-10-23 23:47:18 +0530

    DSA_Array_Revision

--- src/Arrays_Easy_Medium_Hard/RotateElementByDPlaces.java
+++ src/Arrays_Easy_Medium_Hard/RotateElementByDPlaces.java
@@ -5,25 +5,20 @@ public class RotateElementByDPlaces {
 
 //        int n = k % arr.length;
 //        for (int j = 0; j < n; j++) {
-
 //            int temp = arr[arr.length - 1];
 //            for (int i = arr.length - 2; i >= 0; i--) {
-
 //                arr[i + 1] = arr[i];
 //            }
-
 //            arr[0] = temp;
 //        }
 
         int n = arr.length;
         k = k % n;
-
         rev(arr, 0, n - k - 1);
         rev(arr, n - k, n - 1);
         rev(arr, 0, n - 1);
 
         for (int i : arr) {
-
             System.out.println(i);
         }
     }
