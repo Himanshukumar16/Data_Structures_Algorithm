9fc7413bfce0c34dee42c9b6aa4a532af5cffb96
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-11-06 22:34:46 +0530

    problem_solution

--- src/Arrays_Easy_Medium_Hard/AlternatePositiveAndNegativeInArray.java
+++ src/Arrays_Easy_Medium_Hard/AlternatePositiveAndNegativeInArray.java
@@ -1,7 +1,38 @@
 package Arrays_Easy_Medium_Hard;
 
 public class AlternatePositiveAndNegativeInArray {
-    public void alternatePositiveAndNegativeInArray(int... arr){
+    public void alternatePositiveAndNegativeInArray(int... arr) {
 
+//        Brute-Force -> TC-> O(n) + O(n/2) ,SC-> O(n)
+
+//        int n = arr.length;
+//        int[] pos = new int[n / 2];
+//        int[] neg = new int[n / 2];
+//        int pind = 0;
+//        int nind = 0;
+
+//        for (int j : arr) {
+//            if (j > 0) {
+//                pos[pind] = j;
+//                pind++;
+//            } else {
+//                neg[nind] = j;
+//                nind++;
+//            }
+//        }
+
+//        for (int i = 0; i < n / 2; i++) {
+//            arr[2 * i] = pos[i];
+//            arr[2 * i + 1] = neg[i];
+//        }
+
+
+
+
+
+
+        for (int i : arr) {
+            System.out.println(i);
+        }
     }
 }
