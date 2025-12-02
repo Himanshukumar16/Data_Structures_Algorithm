1b81019dabff4a45811860b4a2bec27262abceca
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-12-02 23:30:21 +0530

    solution

--- src/Arrays_Easy_Medium_Hard/LongestConsecutiveSequence.java
+++ src/Arrays_Easy_Medium_Hard/LongestConsecutiveSequence.java
@@ -3,5 +3,25 @@ package Arrays_Easy_Medium_Hard;
 public class LongestConsecutiveSequence {
     public void longestConsecutiveSequence(int[] nums) {
 
+        int longest = 1;
+        for (int j : nums) {
+            int num = j;
+            int count = 1;
+            while (ls(nums, num + 1)) {
+                num++;
+                count++;
+            }
+            longest = Math.max(longest, count);
+        }
+        System.out.println(longest);
+    }
+
+    private boolean ls(int[] nums, int num) {
+        for (int j : nums) {
+            if (j == num) {
+                return true;
+            }
+        }
+        return false;
     }
 }
