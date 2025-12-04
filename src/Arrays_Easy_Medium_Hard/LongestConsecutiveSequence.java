827a2efc96664ab04b35b27d2aa15b7de7dd415a
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-12-04 23:41:14 +0530

    solution

--- src/Arrays_Easy_Medium_Hard/LongestConsecutiveSequence.java
+++ src/Arrays_Easy_Medium_Hard/LongestConsecutiveSequence.java
@@ -3,24 +3,48 @@ package Arrays_Easy_Medium_Hard;
 public class LongestConsecutiveSequence {
     public void longestConsecutiveSequence(int[] nums) {
 
-        int longest = 0;
-        for (int j : nums) {
-            int count = 1;
-            while (ls(nums, j + 1)) {
-                j++;
-                count++;
-            }
-            longest = Math.max(longest, count);
+//        Brute :->
+//        int longest = 0;
+//        for (int j : nums) {
+//            int count = 1;
+//            while (ls(nums, j + 1)) {
+//                j++;
+//                count++;
+//            }
+//            longest = Math.max(longest, count);
+//        }
+//        System.out.println(longest);
+
+
+//        Better :->
+
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = 0; j < nums.length - i - 1; j++) {
+//                if (nums[j] > nums[j + 1]) {
+//                    int temp = nums[j];
+//                    nums[j] = nums[j + 1];
+//                    nums[j + 1] = temp;
+//                }
+//            }
+//        }
+//        int longest = 0;
+//        int cursmall = Integer.MIN_VALUE;
+//        int count = 1;
+
+
+
+        for(int i : nums) {
+            System.out.print(i+" ");
         }
-        System.out.println(longest);
     }
 
-    private boolean ls(int[] nums, int num) {
-        for (int j : nums) {
-            if (j == num) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private boolean ls(int[] nums, int num) {
+//        for (int j : nums) {
+//            if (j == num) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+
 }
