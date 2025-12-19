9b96ecabf7de54e35af413ceea3fa8d057f1be74
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-12-19 23:58:25 +0530

    sol

--- src/Arrays_Easy_Medium_Hard/ShuffleTheArray.java
+++ src/Arrays_Easy_Medium_Hard/ShuffleTheArray.java
@@ -3,10 +3,10 @@ package Arrays_Easy_Medium_Hard;
 public class ShuffleTheArray {
     public void ShuffleTheArray(int[] nums,int n) {
 //        TC -> O(n), SC -> O(n)...
+
         int[] odd = new int[nums.length / 2];
         int[] even = new int[nums.length / 2];
         int ind = 0;
-
         for (int i = 0; i < nums.length; i++) {
             if (i < n) odd[i] = nums[i];
             else {
@@ -14,10 +14,8 @@ public class ShuffleTheArray {
                 ind++;
             }
         }
-
         int oind = 0;
         int eind = 0;
-
         for (int i = 0; i < nums.length; i++) {
             if (i % 2 == 0) {
                 nums[i] = odd[oind];
@@ -27,7 +25,6 @@ public class ShuffleTheArray {
                 eind++;
             }
         }
-
         for (int i : nums) {
             System.out.println(i);
         }
