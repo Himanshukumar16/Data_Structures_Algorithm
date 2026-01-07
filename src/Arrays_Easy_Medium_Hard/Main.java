a9ebf4334fbdef4c94a74e490e0c3705b76aca83
Himanshu <himanshu.k.upadhyay05@gmail.com>
2026-01-08 00:31:40 +0530

    Rotate-Image.

--- src/Arrays_Easy_Medium_Hard/Main.java
+++ src/Arrays_Easy_Medium_Hard/Main.java
@@ -199,8 +199,9 @@ public class Main {
 
         RotateImage r = new RotateImage();
         r.rotateImage(new int[][]{
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}});
+                {5,1,9,11},
+                {2,4,8,10},
+                {13,3,6,7},
+                {15,14,12,16}});
     }
 }
--- src/Arrays_Easy_Medium_Hard/RotateImage.java
+++ src/Arrays_Easy_Medium_Hard/RotateImage.java
@@ -2,6 +2,10 @@ package Arrays_Easy_Medium_Hard;
 
 public class RotateImage {
     public void rotateImage(int[][] matrix) {
+
+//        Brute : TC -> O(n^2), SC -> O(n^2)...
+
+
 //        int[][] arr = new int[matrix.length][matrix[0].length];
 //        for (int i = 0; i < matrix.length; i++) {
 //            for (int j = 0; j < matrix[i].length; j++) {
@@ -16,22 +20,33 @@ public class RotateImage {
 //        }
 
 
-//        optimal Solution :
+//        optimal Solution : TC -> O(n^2), SC -> O(1)...
 
-        for(int i = 0; i < matrix.length - 1; i++) {
-             for(int j = i + 1; j < matrix[i].length; j++) {
-                 int temp = matrix[i][j];
-                 matrix[i][j] = matrix[j][i];
-                 matrix[j][i] = temp;
-             }
-        }
-        for (int i = 0; i < matrix.length/2; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
 
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            int index = matrix[i].length - 1;
+
+            for (int j = 0; j < matrix[i].length / 2; j++) {
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][index];
+                matrix[i][index] = temp;
+                index--;
+            }
+        }
+
         for (int[]elements : matrix) {
             for (int element : elements) {
+
                 System.out.print(element+" ");
             }
             System.out.println();
