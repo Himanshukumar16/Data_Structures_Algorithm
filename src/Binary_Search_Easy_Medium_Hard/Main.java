6cfdcdcd2d71ce35e4d113007b065e0edc0cf367
Himanshu <himanshu.k.upadhyay05@gmail.com>
2026-01-30 23:26:14 +0530

    Implement-Lower-Bound.

--- src/Binary_Search_Easy_Medium_Hard/Main.java
+++ src/Binary_Search_Easy_Medium_Hard/Main.java
@@ -3,7 +3,11 @@ package Binary_Search_Easy_Medium_Hard;
 public class Main {
     public static void main(String[] args) {
 
-        BinarySearchToFindNInSortedArray b = new BinarySearchToFindNInSortedArray();
-        b.binarySearchToFindNInSortedArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},8);
+//        BinarySearchToFindNInSortedArray b = new BinarySearchToFindNInSortedArray();
+//        b.binarySearchToFindNInSortedArray(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},8);
+
+//        ImplementLowerBound i = new ImplementLowerBound();
+//        i.implementLowerBound(new int[]{2, 3, 7, 10, 11, 11, 25},2);
+
     }
 }
