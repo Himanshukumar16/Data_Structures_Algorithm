e494e46223713347a6196f441a7ed9b79a5843fb
Himanshukumar16 <himanshu.k.upadhyay05@gmail.com>
2026-04-07 23:55:33 +0530

    intersection-in-ll.

--- src/LinkedList/Questions/IntersectionOfAll.java
+++ src/LinkedList/Questions/IntersectionOfAll.java
@@ -27,37 +27,54 @@ public class IntersectionOfAll {
 //        }
 //        System.out.println("null");
 
-//        optimal: TC -> O(n1+n2), SC -> O(1).
+//        TC -> O(n1+n2), SC -> O(1).
 
-        int lengthOf1 = 0;
-        int lengthOf2 = 0;
+//        int lengthOf1 = 0;
+//        int lengthOf2 = 0;
+//        NodeForSLL temp1 = head1;
+//        NodeForSLL temp2 = head2;
+//
+//        while (temp1 != null) {
+//            lengthOf1++;
+//            temp1 = temp1.next;
+//        }
+//
+//        while (temp2 != null) {
+//            lengthOf2++;
+//            temp2 = temp2.next;
+//        }
+//
+//        if (lengthOf1 < lengthOf2) {
+//            System.out.println(collisionPoint(head1, head2, lengthOf2 - lengthOf1));
+//        } else System.out.println(collisionPoint(head2, head1, lengthOf1 - lengthOf2));
+
+//        Tc -> O(), SC -> O(1).
+
         NodeForSLL temp1 = head1;
         NodeForSLL temp2 = head2;
 
-        while (temp1 != null) {
-            lengthOf1++;
+        while (temp1 !=  temp2) {
+            if (temp1 == null && temp2 == null) {
+                System.out.println("null");
+                return;
+            }
+            if (temp1 == null) temp1 = head2;
+            if (temp2 == null) temp2 = head1;
             temp1 = temp1.next;
-        }
-
-        while (temp2 != null) {
-            lengthOf2++;
             temp2 = temp2.next;
         }
-
-        if (lengthOf1 < lengthOf2) {
-            System.out.println(collisionPoint(head1, head2, lengthOf2 - lengthOf1));
-        } else System.out.println(collisionPoint(head2, head1, lengthOf1 - lengthOf2));
+        System.out.println(temp1);
     }
 
-    private NodeForSLL collisionPoint(NodeForSLL back, NodeForSLL front, int difference) {
+    private NodeForSLL collisionPoint(NodeForSLL smallerList, NodeForSLL biggerList, int difference) {
 
         for (int i = 0; i < difference; i++) {
-            back = back.next;
+            biggerList = biggerList.next;
         }
-        while (front != back) {
-            front = front.next;
-            back = back.next;
+        while (smallerList != biggerList) {
+            biggerList = biggerList.next;
+            smallerList = smallerList.next;
         }
-        return back;
+        return smallerList;
     }
 }
